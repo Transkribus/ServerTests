0bc6eba19cccfec1f0b0c1b06f4fb2a5030c7d32
kahlep <philip.kahle@uibk.ac.at>
2017-10-12 10:33:33 +0200

    initial commit

---
+++ src/main/java/org/dea/ProxyTester/App.java
@@ -0,0 +1,97 @@
+package org.dea.ProxyTester;
+
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.InetSocketAddress;
+import java.net.MalformedURLException;
+import java.net.Proxy;
+import java.net.Proxy.Type;
+import java.net.SocketAddress;
+import java.net.URL;
+
+import org.apache.commons.io.FileUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Utility for testing Java proxy settings on UIBK servers
+ * 
+ * @author philip
+ *
+ */
+public class App {
+	private static final Logger logger = LoggerFactory.getLogger(App.class);
+	
+	private static final boolean USE_LOGGER = true;
+	
+	public static void main(String[] args) {
+		log("http.proxyHost = " + System.getProperty("http.proxyHost"));
+		log("http.proxyPort = " + System.getProperty("http.proxyPort"));
+		log("http.nonProxyHosts = " + System.getProperty("http.nonProxyHosts"));
+		log("https.proxyHost = " + System.getProperty("https.proxyHost"));
+		log("https.proxyPort = " + System.getProperty("https.proxyPort"));
+		
+		String urlStr = "https://archive.thulb.uni-jena.de/ufb/servlets/MCRDFGServlet/MAX/ufb_derivate_00003259/Chart-B-00024_000.tif";
+		if(args != null && args.length > 0) {
+			urlStr = args[0];
+		}
+		log("==================================================================");
+		URL url;
+		try {
+			url = new URL(urlStr);
+		} catch (MalformedURLException e) {
+			log("ERROR: Given URL is not valid: " + urlStr, e);
+			return;
+		}
+		log("Connectivity test with URL: " + urlStr);
+		File out = new File("/tmp/proxyTesterTmpFile");
+		out.deleteOnExit();
+		
+		log("Trying plain download...");
+		
+		try {
+			FileUtils.copyURLToFile(url, out);
+			log("Success!");
+		} catch (IOException e) {
+			log("ERROR: Could not download file at URL: " + urlStr, e);
+		}
+		log("Trying download with explicit proxy...");
+		try {
+			Proxy proxy = getHttpProxy();
+			InputStream source = url.openConnection(proxy).getInputStream();
+			FileUtils.copyInputStreamToFile(source, out);
+		} catch (IOException e) {
+			log("ERROR: Could not download file at URL: " + urlStr, e);
+			return;
+		}
+		log("Success! Proxy settings work.");
+	}
+	
+	public static Proxy getHttpProxy() {
+		SocketAddress sa = new InetSocketAddress("proxy.uibk.ac.at", 3128);
+		Proxy p = new Proxy(Type.HTTP, sa);
+		return p;
+	}
+	
+	private static void log(final String text, Throwable t) {
+		if(t == null) {
+			log(text);
+			return;
+		}
+		if(USE_LOGGER) {
+			logger.error(text, t);
+		} else {
+			System.out.println(text);
+			t.printStackTrace();
+		}
+	}
+	
+	private static void log(final String text) {
+		if(USE_LOGGER) {
+			logger.info(text);
+		} else {
+			System.out.println(text);
+		}
+	}
+}
